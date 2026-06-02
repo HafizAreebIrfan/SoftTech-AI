b75494c9267f6a6247fb035193a31bc9a148388f
Hafiz Areeb <hafizareebirfan@gmail.com>
2026-06-03 02:06:24 +0500

    Implement global frontend Socket.io service and connection lifecycle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useApplyGlobalThemeVars } from "../infrastructure/store/themeStore";
-import { RouterProvider } from '@tanstack/react-router'
+import { RouterProvider } from '@tanstack/react-router';
 import { router } from "../infrastructure/routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
+import { useAuthStore } from "../infrastructure/store/authStore";
+import { socket } from "../infrastructure/socket/socketService";
 import "react-toastify/dist/ReactToastify.css";
 
 const App: React.FC = () => {
   useApplyGlobalThemeVars();
+  const { isAuthenticated } = useAuthStore();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // Connect to the socket server
+      socket.connect();
+
+      const onConnect = () => {
+        console.log("Socket connected on frontend:", socket.id);
+      };
+
+      const onConnectError = (error: Error) => {
+        console.error("Socket connection error:", error);
+      };
+
+      socket.on("connect", onConnect);
+      socket.on("connect_error", onConnectError);
+
+      return () => {
+        socket.off("connect", onConnect);
+        socket.off("connect_error", onConnectError);
+        socket.disconnect();
+      };
+    } else {
+      socket.disconnect();
+    }
+  }, [isAuthenticated]);
 
   return (
     <>
---
+++ src/infrastructure/socket/socketService.ts
@@ -0,0 +1,11 @@
+import { io, Socket } from "socket.io-client";
+import { env } from "../config/env";
+
+const SOCKET_URL = window.location.hostname === "localhost"
+  ? "http://localhost:4100"
+  : env.devServer;
+
+export const socket: Socket = io(SOCKET_URL, {
+  autoConnect: false,
+  withCredentials: true,
+});
