9eeb6242be2c8394e172ff10b39be63d0011baa6
Hafiz Areeb <hafizareebirfan@gmail.com>
2026-06-03 02:17:51 +0500

    Socket integration

--- src/infrastructure/socket/socketService.ts
+++ src/infrastructure/socket/socketService.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 import { env } from "../config/env";
 
 const SOCKET_URL = window.location.hostname === "localhost"
-  ? "http://localhost:4100"
+  ? env.devServer
   : env.devServer;
 
 export const socket: Socket = io(SOCKET_URL, {
