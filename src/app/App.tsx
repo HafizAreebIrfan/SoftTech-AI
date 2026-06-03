1b604bfacf5f98d656b2d9e7db54695a2033b0f1
Hafiz Areeb <hafizareebirfan@gmail.com>
2026-06-03 19:33:43 +0500

    Vercel Integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from "react-toastify";
 import { useAuthSync } from "../hooks";
 import "react-toastify/dist/ReactToastify.css";
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
   useApplyGlobalThemeVars();
