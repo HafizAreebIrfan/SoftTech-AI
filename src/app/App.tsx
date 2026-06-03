b08c0ebd979c02e160efc11ad328c093191835ad
Hafiz Areeb <hafizareebirfan@gmail.com>
2026-06-04 01:36:39 +0500

    Remove vercel insights

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,8 +6,6 @@ import { ToastContainer } from "react-toastify";
 import { useAuthSync } from "../hooks";
 import { useAuthStore } from "../infrastructure/store/authStore";
 import "react-toastify/dist/ReactToastify.css";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   useApplyGlobalThemeVars();
@@ -30,8 +28,6 @@ const App: React.FC = () => {
     <>
       <RouterProvider router={router} />
       <ToastContainer />
-      <SpeedInsights />
-      <Analytics />
     </>
   );
 };
