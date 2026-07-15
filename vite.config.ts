ee6e731cf295ba557bf3ddafbc0b4f717486d261
Hafiz Areeb <hafizareebirfan@gmail.com>
2026-07-15 21:58:20 +0500

    logs and implementation

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "https://softtech-ai-app.onrender.com/",
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
