cc289cd73aedea17f6c460660c870021b9ef2799
Hafiz Areeb <122298711+HafizAreebIrfan@users.noreply.github.com>
2026-07-15 22:54:24 +0500

    Delete dist directory

--- dist/assets/Widgets-CAX-sIWE.js
+++
@@ -1,1 +0,0 @@
-import{ew as t,fl as e,fV as r,aU as o}from"./index-yzaOqzqS.js";t.createRoot(document.getElementById("root")).render(e.jsxs(r.StrictMode,{children:[e.jsx("div",{style:{color:"white",background:"#222",padding:"20px",fontSize:"20px"},children:"React Mounted"}),e.jsx(o,{})]}));
