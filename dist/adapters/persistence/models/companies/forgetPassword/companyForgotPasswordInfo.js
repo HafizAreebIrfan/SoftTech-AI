4ec3ef80dc28163846ff40924e099788b4d044b5
Hafiz Areeb <122298711+HafizAreebIrfan@users.noreply.github.com>
2026-07-15 22:54:52 +0500

    Delete dist directory

--- dist/adapters/persistence/models/companies/forgetPassword/companyForgotPasswordInfo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CompanyForgotPasswordFields = void 0;
-exports.CompanyForgotPasswordFields = {
-    passwordResetOTP: { type: String },
-    passwordResetOTPExpires: { type: Date },
-};
